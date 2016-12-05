5381cf283cb24ef8009594c55dc736870d7a153a
Beiwen Liu <bliu85@gatech.edu>
2016-12-05 02:36:38 -0500

    Detect Time and Key Signature and create new Stream

--- Mash.py
+++ Mash.py
@@ -3,7 +3,7 @@
 """
 Created on Thu Nov  3 23:35:49 2016
 
-@author: MacbookRetina
+@author: Beiwen Liu
 """
 import music21
 from music21 import *
@@ -23,8 +23,12 @@ def switch():
     #s.show()
     
 def determineChord(measure):
+    #measure.notes[x].offset for offset
     for x in range(0,len(measure.notes)):
-        print measure.notes[x]
+        print "chord" + str(measure.notes[x].offset)
+def determineMelody(measure):
+    for x in range(0,len(measure.notes)):
+        print "melody" + str(measure.notes[x].offset)
     
 def findScale():
     c2 = chord.Chord(['g','e-','b-'])
@@ -49,8 +53,56 @@ def main():
         if (len(chord.measure(x).notes) == 0 and len(melody.measure(x).notes) == 0):
             break
         determineChord(chord.measure(x))
+        determineMelody(melody.measure(x))
         
-        
+def streamCreate():
+    c = chord.Chord(['C4','E4','G4'])
+    n = note.Note('F#4')
+    m = stream.Measure()
+    m.append(n)
+    m.append(c)
+    
+    n = stream.Measure()
+    n.append(n)
+    n.append(c)
+    
+    
+    """
+    p1 = stream.Part()
+    n2 = note.Note('C6')
+    p1.append(n2)
+    p2 = stream.Part()
+    n3 = note.Note('G6')
+    p2.append(n3)"""
+    s = stream.Stream()
+    
+    s.insert(0,m)
+    
+    
+    s.show()
+    
+def reconstruction():
+    c = converter.parse('./ChopinNocturneOp9No2.xml') # Get Info from Original Sheet
+    sc = stream.Score() # New Stream
+    
+    melody = stream.Part() # Melody part
+    chord1 = stream.Part() # Chord part
+    
+    m = stream.Measure() 
+    
+    timeSignature = c.parts[0].measure(1).getContextByClass('TimeSignature') #Get Time Signature
+    keySignature = c.parts[1].measure(1).getContextByClass('KeySignature') #Get Key Signature
+    
+    melody.timeSignature = timeSignature
+    melody.keySignature = keySignature
+    chord1.keySignature = keySignature
+    chord1.timeSignature = timeSignature
+    
+    sc.insert(0,melody)
+    sc.insert(1,chord1)
+    sc.show()
  
-main()   
+reconstruction()
+#streamCreate()
+#main()   
 #findScale()
